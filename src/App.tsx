a15e231f549cb154cfe3928e8a930ad30f3c9854
MaJungHan <wjdgksak@naver.com>
2022-01-27 15:50:32 +0900

    table 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
-import RecommendPage from './pages/recommend';
+import Community from './pages/Community';
+import Recommend from './pages/Recommend';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<></>} />
-        <Route path="/recommend" element={<RecommendPage />} />
+        <Route path="/recommend" element={<Recommend />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/community" element={<></>} />
+        <Route path="/community" element={<Community />} />
         <Route path="/alcoholcup" element={<></>} />
         <Route path="/menu" element={<></>} />
         <Route path="/search" element={<></>} />
