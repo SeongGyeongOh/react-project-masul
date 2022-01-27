cfb0b49fb7f3544d14e0d59c1b8da833d1ad3535
Gyeong <kamniang@naver.com>
2022-01-27 11:30:43 +0900

    less to scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import './App.less';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Button } from 'antd';
 import RecommendPage from './pages/recommend/RecommendPage';
 
 const App = () => {
